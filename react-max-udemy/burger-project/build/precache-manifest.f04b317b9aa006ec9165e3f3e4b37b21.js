53b8273303b57924b67800d143dabe56dd99f53f
Franklin Sales <franklin.salesx@gmail.com>
2020-02-18 23:38:46 -0300

    testing build with PUBLIC_URL

---
+++ react-max-udemy/burger-project/build/precache-manifest.f04b317b9aa006ec9165e3f3e4b37b21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94713d3ea6d4864080f9a6f66c755fe8",
+    "url": "/showcases/react/burger-project/index.html"
+  },
+  {
+    "revision": "733ad5ec8f1266dd8acf",
+    "url": "/showcases/react/burger-project/static/css/main.7ada8a79.chunk.css"
+  },
+  {
+    "revision": "c38d640fc4849175de5a",
+    "url": "/showcases/react/burger-project/static/js/2.2060bee0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/showcases/react/burger-project/static/js/2.2060bee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "733ad5ec8f1266dd8acf",
+    "url": "/showcases/react/burger-project/static/js/main.02b699a0.chunk.js"
+  },
+  {
+    "revision": "751d84af42d33e6fa0a1",
+    "url": "/showcases/react/burger-project/static/js/runtime-main.ef218aad.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/showcases/react/burger-project/static/media/burger-logo.b8503d26.png"
+  }
+]);
--- react-max-udemy/burger-project/build/service-worker.js
+++ react-max-udemy/burger-project/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.162fee52ed0255a64dbe2a8815ecce21.js"
+  "/showcases/react/burger-project/precache-manifest.f04b317b9aa006ec9165e3f3e4b37b21.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/showcases/react/burger-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
