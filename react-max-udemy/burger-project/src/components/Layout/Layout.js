b394f52b0c7f2f0bb630d15124c4ea6f8a45306c
Franklin Sales <franklin.salesx@gmail.com>
2020-02-22 18:17:06 -0300

    improve menu mobile SideDrawer

--- react-max-udemy/burger-project/src/components/Layout/Layout.js
+++ react-max-udemy/burger-project/src/components/Layout/Layout.js
@@ -8,16 +8,22 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 class Layout extends Component {
 
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
     sideDrawerColoseHandler = () => {
         this.setState({ showSideDrawer: false });
     }
 
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer }
+        })
+    }
+
     render () {
         return (<Aux>
-            <Toolbar />
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
             <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerColoseHandler} />
             <main className={classes.Content}>
                 {this.props.children}
