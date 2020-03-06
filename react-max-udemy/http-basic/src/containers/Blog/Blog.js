38135a843ebb0fb8e47978ff6cd61a6dc88e70dd
Franklin Sales <franklin.salesx@gmail.com>
2020-03-05 23:11:05 -0300

    implements redirect

--- react-max-udemy/http-basic/src/containers/Blog/Blog.js
+++ react-max-udemy/http-basic/src/containers/Blog/Blog.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom'
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
 
 import './Blog.css';
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost'
-import FullPost from './FullPost/FullPost'
 
 class Blog extends Component {
     render () {
@@ -16,7 +15,7 @@ class Blog extends Component {
                         <ul>
                             <li>
                                 <NavLink 
-                                    to="/" 
+                                    to="/posts" 
                                     exact
                                     activeClassName="my-active"
                                     activeStyle={{
@@ -39,11 +38,11 @@ class Blog extends Component {
                     /*<Route path="/" exact render={() => <h1>Home</h1>} />
                     <Route path="/" render={() => <h1>Home 2</h1>} />)*/
                 }
-
-                <Route path="/" exact component={Posts} />
-                <Switch>                                        
-                    <Route path="/new-post" exact component={NewPost} />                    
-                    <Route path="/:id" exact component={FullPost} />
+                
+                <Switch>                                                        
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/posts" component={Posts} />
+                    <Redirect path="/" to="/posts"/>
                 </Switch>
             </div>
         );
--- react-max-udemy/http-basic/src/containers/Blog/Posts/Posts.js
+++ react-max-udemy/http-basic/src/containers/Blog/Posts/Posts.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import axios from '../../../axios'
-import { Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 import Post from '../../../components/Post/Post'
 import './Posts.css'
+import FullPost from '../FullPost/FullPost'
 
 class Posts extends Component {
 
@@ -38,8 +39,8 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.props.history.push({pathname: '/' + id})
-        //alternative: this.props.history.push("/" + id)
+        //alternative: this.props.history.push({pathname: '/posts/' + id})
+        this.props.history.push("/posts/" + id)
     }
 
     render () {
@@ -48,19 +49,23 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                    //<Link to={`/${post.id}`} key={post.id}>
+                    //<Link to={`/posts/${post.id}`} key={post.id}>
                         <Post
                             title={post.title}
                             author={post.author}
+                            key={post.id}
                             clicked={() => this.postSelectedHandler(post.id)} />
                     //</Link>
                 )
             })
         }
         return (
-            <section className="Posts">
-                {posts}
-            </section>
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <Route path={this.props.match.url + "/:id"} exact component={FullPost} />
+            </div>
         )
     }
 }
