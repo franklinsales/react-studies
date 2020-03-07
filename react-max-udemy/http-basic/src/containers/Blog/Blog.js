779517d14c8b59b3ebc7f8ee2bab7914559c5284
Franklin Sales <franklin.salesx@gmail.com>
2020-03-07 00:37:58 -0300

    implement lazy load concept

--- react-max-udemy/http-basic/src/containers/Blog/Blog.js
+++ react-max-udemy/http-basic/src/containers/Blog/Blog.js
@@ -3,12 +3,17 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
 
 import './Blog.css';
 import Posts from './Posts/Posts'
-import NewPost from './NewPost/NewPost'
+//import NewPost from './NewPost/NewPost'
+import asyncComponent from '../../hoc/asyncComponent'
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+})
 
 class Blog extends Component {
 
     state = {
-        auth: false
+        auth: true
     }
 
     render () {
@@ -45,10 +50,10 @@ class Blog extends Component {
                 }
                 
                 <Switch>                                                        
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null }
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null }
                     <Route path="/posts" component={Posts} />
-                    <Route render={() => <h1>Not Found</h1>} />
-                    {/*<Redirect path="/" to="/posts"/>*/}
+                    {/*<Route render={() => <h1>Not Found</h1>} />*/}
+                    <Redirect path="/" to="/posts"/>
                 </Switch>
             </div>
         );
