5aad709b196e2334105d3c4d6876c4d9d8fee04c
Franklin Sales <franklin.salesx@gmail.com>
2020-03-12 23:40:45 -0300

    init implementatio of orders page

---
+++ react-max-udemy/burger-project/src/components/Order/Order.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import classes from './Order.module.css';
+
+const order = (props) => (
+    <div>
+        <p>
+            Ingredients: Salad (1)
+        </p>
+        <p>
+            Price <strong> </strong>
+        </p>
+    </div>
+);
+
+export default order;
