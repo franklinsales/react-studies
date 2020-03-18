a1767216e9ddcc0edd5694d7b15130213bb577b9
Franklin Sales <franklin.salesx@gmail.com>
2020-03-18 00:54:42 -0300

    implements validation and visual feedback

--- react-max-udemy/burger-project/src/components/UI/Input/Input.js
+++ react-max-udemy/burger-project/src/components/UI/Input/Input.js
@@ -6,10 +6,16 @@ const input = (props) => {
 
     let inputElement = null
 
+    const inputClasses = [classes.InputElement]
+
+    if(props.invalid && props.shouldValidate){
+        inputClasses.push(classes.Invalid)
+    }
+
     switch (props.elementType) {
         case ('input'):
             inputElement = <input 
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 {...props.elementConfig}
                 value={props.value} 
                 onChange={props.changed}/>
@@ -41,7 +47,7 @@ const input = (props) => {
     
         default:
             inputElement = <input 
-                className={classes.InputElement}
+                className={inputClasses}
                 {...props.elementConfig}
                 value={props.value} 
                 onChange={props.changed}/>
