9bedd10086ea3a9e0e9fd98bee0a217398092a29
Franklin Sales <franklin.salesx@gmail.com>
2020-03-21 17:41:55 -0300

    implements redux, react-redux, settings and first dispatcher

---
+++ react-max-udemy/counter-redux/src/store/reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+    counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+    if (action.type === 'INCREMENT'){
+        return {
+            ...state,
+            counter: ++state.counter
+        }
+    }
+    return state
+}
+
+export default reducer;
