68c757e266b1fd5eb9dd73ba55a8e4d6c0aa0eab
Franklin Sales <franklin.salesx@gmail.com>
2020-03-28 16:28:08 -0300

    resetting the prince

--- react-max-udemy/burger-project/src/store/reducers/burgerBuilder.js
+++ react-max-udemy/burger-project/src/store/reducers/burgerBuilder.js
@@ -42,7 +42,9 @@ const reducer = (state = initialState, action) => {
                     bacon: action.ingredients.bacon,
                     cheese: action.ingredients.cheese,
                     meat: action.ingredients.meat
-                }
+                },
+                totalPrice: 4,
+                error: false
             }
 
         case actionTypes.FECTH_INGREDIENTS_FAILED:
