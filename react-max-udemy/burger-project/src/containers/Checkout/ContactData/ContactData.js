e50d852d048754866764c2165ac8d71f86af2657
Franklin Sales <franklin.salesx@gmail.com>
2020-03-29 11:12:11 -0300

    fix total price errors

--- react-max-udemy/burger-project/src/containers/Checkout/ContactData/ContactData.js
+++ react-max-udemy/burger-project/src/containers/Checkout/ContactData/ContactData.js
@@ -202,7 +202,7 @@ class ContactData extends Component{
 const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
-        price: state.burgerBuilder.totalPrince,
+        price: state.burgerBuilder.totalPrice,
         loading: state.order.loading
     }
 }
