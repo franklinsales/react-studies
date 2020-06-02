1933b78d98efbbdecb383e2d00351e7c125159b6
Franklin Sales <franklin.salesx@gmail.com>
2020-06-01 23:11:49 -0300

    implement UseEffect hook to fetch data of firebase

--- react-max-udemy/react-hooks-explore/src/components/Ingredients/Ingredients.js
+++ react-max-udemy/react-hooks-explore/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientsList from './IngredientList'
@@ -6,6 +6,22 @@ import Search from './Search';
 
 const Ingredients = () => {
   const [ userIngredients, setUserIngredients ] = useState([]);
+
+  useEffect(() => {
+    fetch(process.env.REACT_APP_FIREBASE_URL+'/ingredients.json')
+    .then(response => response.json())
+    .then(responseData => {
+      const loadedIngredients = []
+      for (const key in responseData){
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        })
+      }
+      setUserIngredients(loadedIngredients)
+    })
+  }, [])
 
   const addIngredientHandler = ingredient => {
     fetch(process.env.REACT_APP_FIREBASE_URL+'/ingredients.json', {
